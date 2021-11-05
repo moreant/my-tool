0f8b785f818aaddd582f7493239cdb535999e3fb
molingran <moreant@foxmial.com>
2021-11-06 03:03:28 +0800

    添加备案号

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,30 +29,39 @@
       href: 'https://regex101.com/'
     }
   ]
+
+  const origin = location.origin.indexOf('devify') > 0
 </script>
 
 <template>
-  <Navbar>
-    <!-- Hero -->
-    <Container>
-      <div class="lg:flex lg:items-center lg:justify-between">
-        <h2 class="text-3xl tracking-wide text-white sm:text-4xl">
-          <span class="block mb-1">使用工具何必那么复杂，</span>
-          <span class="block text-blue-300">饱和转换、实时生成、快速复制。</span>
-        </h2>
-      </div>
-    </Container>
-  </Navbar>
-  <Container>
-    <ul class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8 text-center">
-      <li v-for="item in itemList" class="flex min-w-full">
-        <a v-if="item.target === '_blank'" :href="item.href" target="_blank" class="home-item">
-          {{ item.text }}
-        </a>
-        <router-link v-else :to="item.href" class="home-item">
-          {{ item.text }}
-        </router-link>
-      </li>
-    </ul>
-  </Container>
+  <div class="flex flex-col h-full">
+    <Navbar>
+      <!-- Hero -->
+      <Container>
+        <div class="lg:flex lg:items-center lg:justify-between">
+          <h2 class="text-3xl tracking-wide text-white sm:text-4xl">
+            <span class="block mb-1">使用工具何必那么复杂，</span>
+            <span class="block text-blue-300">饱和转换、实时生成、快速复制。</span>
+          </h2>
+        </div>
+      </Container>
+    </Navbar>
+    <div class="flex-grow">
+      <Container>
+        <ul class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8 text-center">
+          <li v-for="item in itemList" class="flex min-w-full">
+            <a v-if="item.target === '_blank'" :href="item.href" target="_blank" class="home-item">
+              {{ item.text }}
+            </a>
+            <router-link v-else :to="item.href" class="home-item">
+              {{ item.text }}
+            </router-link>
+          </li>
+        </ul>
+      </Container>
+    </div>
+    <div v-if="origin" class="text-center mb-4 text-gray-400">
+      <a href="https://beian.miit.gov.cn" class="hover:text-gray-900">粤ICP备2021109792号</a>
+    </div>
+  </div>
 </template>
